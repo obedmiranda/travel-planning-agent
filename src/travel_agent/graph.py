3067b05500e3be0d49739aba9c4f6ee1cacafe4c
obed <obed_mp@hotmail.com>
2026-09-16 15:45:26 -0600

    Add conditional routing after planner

--- src/travel_agent/graph.py
+++ src/travel_agent/graph.py
@@ -6,14 +6,36 @@ from travel_agent.nodes.parse_request import get_request
 from travel_agent.nodes.planner import planner
 from travel_agent.state import InputState, TravelAgentState
 
+
+def route_after_planner(state: TravelAgentState) -> str:
+    if state["can_plan"]:
+        print("ROUTE: can_plan")
+        return "can_plan"
+
+    print("ROUTE: missing_information")
+    return "missing_information"
+
+
 workflow = StateGraph(TravelAgentState, input_schema=InputState)
 
+# Nodes setup
 workflow.add_node("parse_request", get_request)
 workflow.add_node("planner", planner)
 
+# Edges setup
 workflow.add_edge(START, "parse_request")
 workflow.add_edge("parse_request", "planner")
-workflow.add_edge("planner", END)
+
+
+workflow.add_conditional_edges(
+    "planner",
+    route_after_planner,
+    {
+        "can_plan": END,
+        "missing_information": END,
+    },
+)
+
 
 graph = workflow.compile()
 
